9d083c46dd9048bc345715bc287bff623bd3a33a
Daniel Todd <dtodd2079@gmail.com>
2023-10-31 17:46:31 -0700

    got the bookings page to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_211258) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_004216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "buyer_id"
+    t.integer "game_id"
+    t.text "start_date"
+    t.text "end_date"
+    t.text "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "category"
