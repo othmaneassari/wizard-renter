b7b2f3fc40a1369ee05d6c00bd98151784c13881
Daniel Todd <dtodd2079@gmail.com>
2023-10-31 17:32:52 -0700

    created the bookings model, waiting on the schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "index", to: "pages#index"
+  get "index", to: "bookings#index"
   # Defines the root path route ("/")
   # root "articles#index"
   resources :games
+  resources :bookings
 end
