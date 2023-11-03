d394360b68d0798906a12500c5a0bd598aa0c5f7
othmane assari <othmane.assari@gmail.com>
2023-11-03 01:45:36 +0100

    Game model finished

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,4 +7,20 @@ class GamesController < ApplicationController
   def show
     @games = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to game_path(@game.id)
+  end
+end
+
+private
+
+def game_params
+  params.require(:game).permit(:id, :name, :category, :price, :age_range, :rating, :manufacturer)
 end
