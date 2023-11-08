00467e090ba8c6db8105e40435091c82df63b3a1
Daniel Todd <dtodd2079@gmail.com>
2023-11-07 17:28:41 -0800

    working on the bookings model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,31 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = Booking.where(user:current_user)
   end
 
   def show
     @bookings = Booking.find(params[:id])
   end
+
+  def new
+    @bookings = Booking.new
+    @booking.game = Game.find(params[:game_id])
+  end
+
+  def create
+    @booking = booking.new(booking_params)
+    @booking.user = current_user
+    @booking.game = Game.find(params[:game_id])
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+
+
+
+private
+
+def booking_params
+  params.require(:booking).permit(:category, :price, :age_range, :rating, :manufacturer)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "index", to: "bookings#index"
-  get "bookings/:id", to: "bookings#show"
   # Defines the root path route ("/")
   # root "articles#index"
   resources :games
   resources :bookings
-  resources :my_bookings
 end
